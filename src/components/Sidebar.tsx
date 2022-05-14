a09e3cb250967bb77edba833baa571de8d824dc0
Cogentx <code@cogentx.com>
2022-05-14 19:41:08 -0230

    Create Sidebar component | incl HeroIcons and logo

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,19 @@
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from '@heroicons/react/outline';
+
+
+
+
+export default function Sidebar() {
+  return <div>
+    <img src="/logo-twitter.png" alt="twitter logo" />
+  </div>
+}
