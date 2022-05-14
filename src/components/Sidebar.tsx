6d8fcaf8917f8dd126ed150234d0c29d0436d187
Cogentx <code@cogentx.com>
2022-05-14 20:35:10 -0230

    Sidebar | SidebarRow with TS interface and Props

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,19 +1,31 @@
+import Image from 'next/image';
 import {
   BellIcon,
-  HashtagIcon,
   BookmarkIcon,
   CollectionIcon,
   DotsCircleHorizontalIcon,
+  HashtagIcon,
+  HomeIcon,
   MailIcon,
   UserIcon,
-  HomeIcon,
 } from '@heroicons/react/outline';
-
-
-
+import SidebarRow from './SidebarRow';
 
 export default function Sidebar() {
-  return <div>
-    <img src="/logo-twitter.png" alt="twitter logo" />
-  </div>
+  return (
+    <div>
+      <Image
+        width={40}
+        height={40}
+        src="/logo-twitter.png"
+        alt="twitter logo"
+      />
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={HashtagIcon} title="Explore" />
+      <SidebarRow Icon={BellIcon} title="Notifications" />
+      <SidebarRow Icon={MailIcon} title="Messages" />
+      <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+      <SidebarRow Icon={CollectionIcon} title="Lists" />
+    </div>
+  );
 }
