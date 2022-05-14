eeeec66d5bc19c9bf0dbed700d2218b6121cf8b8
Cogentx <code@cogentx.com>
2022-05-14 21:05:23 -0230

    Create Feed Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 
 const Home: NextPage = () => {
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
 
       <main>
         <Sidebar/>
+        <Feed/>
       </main>
     </div>
   )
