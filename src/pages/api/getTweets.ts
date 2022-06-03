0444e26a2da499cf496f50a25d104ea627a604d5
Cogentx <code@cogentx.com>
2022-06-03 10:36:09 -0230

    Create API for getting Tweets from Sanity

---
+++ src/pages/api/getTweets.ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { sanityClient } from '../../../sanity';
+import { Tweet } from '../../../typings';
+import { groq } from 'next-sanity';
+
+type Data = {
+  tweets: Tweet[];
+};
+
+const feedQuery = groq`
+*[_type=="tweet" && !blockTweet] {
+  _id,
+  ...
+} | order(_createdAt desc)
+`;
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse<Data>
+) {
+  const tweets: Tweet[] = await sanityClient.fetch(feedQuery);
+  console.log({ tweets });
+  response.status(response.statusCode).json({ tweets });
+}
