b11e0399399cde992254b317d1651e9b1edf81d2
Cogentx <code@cogentx.com>
2022-06-03 16:47:41 -0230

    Complete Tweet Comp styling including custom class

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,4 +1,10 @@
 import TimeAgo from 'react-timeago';
+import {
+  ChatAlt2Icon,
+  HeartIcon,
+  SwitchHorizontalIcon,
+  UploadIcon,
+} from '@heroicons/react/outline';
 import { ITweet } from '../../typings';
 
 interface IProps {
@@ -21,22 +27,37 @@ export default function Tweet({ tweet }: IProps) {
             <p className="hidden text-sm text-gray-500 sm:inline">
               @{tweet.username.replace(/\s+/g, '').toLowerCase()} ·
             </p>
-
             <TimeAgo
               className="text-sm text-gray-500"
               date={tweet._createdAt}
             />
           </div>
 
-          <p>{tweet.text}</p>
+          <p className="pt-1">{tweet.text}</p>
 
           {tweet.image && (
             <img
-              className="m-5 ml-0 mb-1 rounded-lg object-cover shadow-sm"
+              className="m-5 ml-0 max-h-60 rounded-lg object-cover shadow-sm"
               src={tweet.image}
               alt="tweet image"
             />
           )}
+
+          <div className="m-5 ml-0 flex justify-between">
+            <div className="tweetButton">
+              <ChatAlt2Icon className="h-5 w-5" />
+              <p>5</p>
+            </div>
+            <div className="tweetButton">
+              <SwitchHorizontalIcon className="h-5 w-5" />
+            </div>
+            <div className="tweetButton">
+              <HeartIcon className="h-5 w-5" />
+            </div>
+            <div className="tweetButton">
+              <UploadIcon className="h-5 w-5" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
