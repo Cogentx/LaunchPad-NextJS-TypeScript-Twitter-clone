8d373f68271ff6a45105c5eddaeefcf5f0d1baa9
Cogentx <code@cogentx.com>
2022-06-03 15:34:42 -0230

    Add React-TimeAgo and Style Tweet

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import TimeAgo from 'react-timeago';
 import { ITweet } from '../../typings';
 
 interface IProps {
-  tweet:ITweet;
+  tweet: ITweet;
 }
 
-export default function Tweet({ tweet}:IProps) {
+export default function Tweet({ tweet }: IProps) {
   return (
-    <div>{tweet.text}</div>
-  )
+    <div className="flex flex-col space-x-3 border-y border-gray-100 p-5">
+      <div className="flex space-x-3">
+        <img
+          className="h-10 w-10 rounded-full object-cover"
+          src={tweet.profileImage}
+          alt="Profile Image"
+        />
+
+        <div>
+          <div className="flex items-center space-x-1">
+            <p className="mr-1 font-bold">{tweet.username}</p>
+            <p className="hidden text-sm text-gray-500 sm:inline">
+              @{tweet.username.replace(/\s+/g, '').toLowerCase()} ·
+            </p>
+
+            <TimeAgo
+              className="text-sm text-gray-500"
+              date={tweet._createdAt}
+            />
+          </div>
+
+          <p>{tweet.text}</p>
+
+          {tweet.image && (
+            <img
+              className="m-5 ml-0 mb-1 rounded-lg object-cover shadow-sm"
+              src={tweet.image}
+              alt="tweet image"
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
