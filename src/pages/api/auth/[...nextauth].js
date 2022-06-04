aa7737f8af13e0b2bc53030244855e89010450d7
Cogentx <code@cogentx.com>
2022-06-04 21:13:51 -0230

    setup Twitter as provider

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+import NextAuth from 'next-auth';
+// import GoogleProvider from 'next-auth/providers/google';
+import TwitterProvider from 'next-auth/providers/twitter';
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    // }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
+      version: '2.0', // opt-in to Twitter OAuth 2.0
+    }),
+    // add more providers here]
+  ],
+});
