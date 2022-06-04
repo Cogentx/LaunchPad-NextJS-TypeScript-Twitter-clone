cdc1588c182d7900d4256d6da346aaa362dd7220
Cogentx <code@cogentx.com>
2022-06-04 21:15:13 -0230

    Refactor Sidebar and SidebarRow componets for Auth

--- src/components/SidebarRow.tsx
+++ src/components/SidebarRow.tsx
@@ -3,13 +3,19 @@ import React, { SVGProps } from 'react';
 interface IProps {
   Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
   title: string;
+  onClick?: () => void;
 }
 
-export default function SidebarRow({ Icon, title }: IProps) {
+export default function SidebarRow({ Icon, title, onClick }: IProps) {
   return (
-    <div className="group flex max-w-fit cursor-pointer items-center space-x-2 rounded-full px-4 py-3 transition-all duration-200 hover:bg-gray-100">
+    <div
+      onClick={() => onClick?.()}
+      className="group flex max-w-fit cursor-pointer items-center space-x-2 rounded-full px-4 py-3 transition-all duration-200 hover:bg-gray-100"
+    >
       <Icon className="h-6 w-6" />
-      <p className="hidden group-hover:text-twitter md:inline-flex font-light text-base lg:text-xl">{title}</p>
+      <p className="hidden text-base font-light group-hover:text-twitter md:inline-flex lg:text-xl">
+        {title}
+      </p>
     </div>
   );
 }
